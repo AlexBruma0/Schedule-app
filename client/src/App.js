2460f0e465573f4eaee730d03865d071b88d9e0a
AlexBruma0 <105242355+AlexBruma0@users.noreply.github.com>
2023-04-23 13:20:57 -0700

    resizing

--- client/src/App.js
+++ client/src/App.js
@@ -18,8 +18,8 @@ import {
 import { Input } from "@chakra-ui/react";
 const remote = "https://myproject-382821.uc.r.appspot.com/";
 
-const local = "http://localhost:8081/"
-var uri = local;
+//const local = "http://localhost:8081/"
+var uri = remote;
 function App() {
   const [items, setItems] = useState([]);
   const [item, setItem] = useState({ start: "", end: "", date: "" , id: "", hours:0});
@@ -62,11 +62,11 @@ function App() {
   };
   return (
     <div className="App">
-      <header className="App-header">
-        <Heading ml = '-29%'>Add</Heading>
-        <Box ml = '-4%'>
+      <Box className="App-header" border='1px'>
+        <Heading float='left' textAlign='left'>Add</Heading>
+        <Box  float='left'>
         <form onSubmit={post}>
-          <Box display="flex" m="7%">
+          <Box display="flex" >
             <Input
               placeholder="From"
               variant="unstyled"
@@ -110,8 +110,8 @@ function App() {
         </Box>
         
 
-        <Heading ml = '-6.5%'>Pay period: April 24 - May 8</Heading>
-        <TableContainer ml = '-7%'>
+        <Heading mt = '3%' textAlign='left' >Pay period: April 24 - May 8</Heading>
+        <TableContainer >
           <Table variant="striped" colorScheme="blackAlpha" size="md">
             <Thead color="red">
               <Tr color="red" fontSize="md">
@@ -135,17 +135,14 @@ function App() {
                       
                       >❌</Button>
                     </Td>
-                    {/* <Td>
-                      <Button colorScheme="linkedin ">✏️</Button>
-                    </Td> */}
                   </Tr>
                 );
               })}
             </Tbody>
           </Table>
         </TableContainer>
-        <Heading ml = '-27.5%'>Total</Heading>
-        <TableContainer ml = '-23%'>
+        <Heading mt = '3%' textAlign='left'>Total</Heading>
+        <TableContainer >
           <Table variant="striped" colorScheme="blackAlpha" size="md">
             <Thead color="red">
               <Tr color="red" fontSize="md">
@@ -161,7 +158,8 @@ function App() {
             </Tbody>
           </Table>
         </TableContainer>
-      </header>
+      </Box>
+      
     </div>
   );
 }
